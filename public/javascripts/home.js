baa108ef4c35c4eade1aa6e11851ba985f1171bc
Xing Wu <xingwu.cs@gmail.com>
2014-10-21 02:39:22 -0400

    home page modified

---
+++ public/javascripts/home.js
@@ -0,0 +1,23 @@
+$(function() {
+    $('#roseBtn').animo({animation: 'tada', duration: 2, iterate: "infinite", keep:true, timing:"ease-in-out"});
+    $('#roseBtn').on('click', function(e) {
+        var rn = $('#roseNum');
+        rn.text(parseInt(rn.text())+1);
+        var roseInstance = document.createElement('img');
+        roseInstance.src = '/images/rose.png';
+        roseInstance.style.top = '0';
+        roseInstance.style.left= Math.floor(Math.random()*80) + '%';
+
+        $('#roseCanvas').append(roseInstance);
+
+        $(roseInstance).animo({
+            animation: 'rotateFadeOutDown'+(Math.random()>0.5?1:2),
+            duration: 5,
+            iterate: 1,
+            keep:true,
+            timing:"linear"
+        }, function() {
+            $(roseInstance).remove();
+        })
+    })
+});
