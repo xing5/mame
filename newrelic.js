0dfe171a0422ac96e15f35230f68c2ee5b4cc20a
Xing Wu <xingwu.cs@gmail.com>
2014-10-21 13:07:00 -0400

    add relic stats

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['xmame'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'f59f04e2e331f849f5621e6a12af97a619c82b2a',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
