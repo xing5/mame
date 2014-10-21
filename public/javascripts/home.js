d0ba2039a86588a3c045c704560c427b87cd0457
Xing Wu <xingwu.cs@gmail.com>
2014-10-21 16:00:01 -0400

    rose button adjustment.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,8 +1,22 @@
 $(function() {
-    $('#roseBtn').animo({animation: 'tada', duration: 2, iterate: "infinite", keep:true, timing:"ease-in-out"});
+    // $('a.btn-danger').animo({
+    //         animation: 'flash',
+    //         duration: 2,
+    //         iterate: "infinite",
+    //         keep:true,
+    //         timing:"ease-in-out"
+    // });
     $('#roseBtn').on('click', function(e) {
-        var rn = $('#roseNum');
-        rn.text(parseInt(rn.text())+1);
+        $('#roseIcon').animo({
+            animation: 'tada',
+            duration: 2,
+            iterate: 1,
+            keep:false,
+            timing:"ease-in-out"
+        }, function() {
+            var rn = $('#roseNum');
+            rn.text(parseInt(rn.text())+1);
+        });
         var roseInstance = document.createElement('img');
         roseInstance.src = '/images/rose.png';
         roseInstance.style.top = '0';
