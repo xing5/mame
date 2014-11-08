6b3d0bd540be76ec1182591cb2a625537f34e535
Xing Wu <xingwu.cs@gmail.com>
2014-11-08 16:36:37 -0500

    deploy bug fixed

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 jade views/{index,her,me,story,letter}.jade -P -o deploy/
 sed -i '' -e 's/\(href="\/\)\([^"]*\)/\1\2\.html/g' deploy/index.html
-grep '/proposal/' deploy/javascripts/*.js | awk -F ':' '{print $1}'|uniq | xargs -I xx sed -i '' -e "s/'\/proposal/'http:\/\/api.xingwu.me\/proposal/g" xx
+grep '/proposal/' deploy/javascripts/*.js | awk -F ':' '{print $1}'|uniq | xargs -I xx sed -i '' -e "s/\"\/proposal/\"http:\/\/api.xingwu.me\/proposal/g" xx
 cp -rf public/images deploy/
 cp -rf public/*.html deploy/
