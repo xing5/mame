9bafba1a0d496ce1118097ff83c3adff32d083a6
Xing Wu <xingwu.cs@gmail.com>
2014-11-10 00:48:16 -0500

    add 1s to slide interval

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('#carouselStory').carousel({interval: 5000, wrap: false});
+    $('#carouselStory').carousel({interval: 6000, wrap: false});
     $('#carouselStory').on('slid.bs.carousel', function (e) {
         if($('#lastPage').hasClass('active')) {
             $('#rightBtn').click(function(event){
