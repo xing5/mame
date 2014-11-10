b59297eb4d3681748782ebdad3dabb34c4ec18e8
Xing Wu <xingwu.cs@gmail.com>
2014-11-10 18:12:54 -0500

    js files location bug fix

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ sed -i '' -e 's/\(href="\/\)\([^"]*\)/\1\2\.html/g' deploy/index.html
 grep '/proposal/' deploy/javascripts/*.js | awk -F ':' '{print $1}'|uniq | xargs -I xx sed -i '' -e "s/\"\/proposal/\"http:\/\/api.xingwu.me\/proposal/g" xx
 cp -rf public/images deploy/
 cp -rf public/*.html deploy/
-cp -rf public/javascripts/*.js deploy/
+cp -rf public/javascripts/*.js deploy/javascripts/
